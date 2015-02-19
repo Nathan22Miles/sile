47e8136793b0642dcddf723bd89bdbbef6a93296
Nathan Miles <nmiles@biblesocieties.org>
2015-02-18 22:27:53 -0600

    working on page breaks

--- lectionary/usxToSile.py
+++ lectionary/usxToSile.py
@@ -9,6 +9,17 @@ import re
 # run this program
 # Use xml editor to verify that xml is well formed
 
+def processSAM(text):
+	parts = text.split("\n")
+	firstS1 = True
+	for i in range(len(parts)-1):
+		if re.match(r"\s*<s1>", parts[i]):
+			if firstS1: 
+				firstS1 = False
+				continue
+			parts[i] = parts[i] + "\n<par/><goodbreak/>"
+	return "\n".join(parts)
+
 with codecs.open("096XXD.usx", "r", "utf-8") as fd:
     text = fd.read()
 
@@ -31,8 +42,8 @@ text = re.sub(r'&lt;', ur'\u2018', text)
 text = re.sub(r'&gt;', ur'\u2019', text)
 
 # insert thin space between single and double quotes
-text = re.sub(r'\u201c *\u2018', ur'\u201c\u2009\u2018', text)
-text = re.sub(r'\u2019 *\u201d', ur'\u2019\u2009\u201d', text)
+text = re.sub(r'\u201c *\u2018', ur'\u201c<glue width="0.2em" />\u2018', text)
+text = re.sub(r'\u2019 *\u201d', ur'\u2019<glue width="0.2em" />\u201d', text)
 
 text = re.sub(ur'\u00e7', ur'\u0268', text)
 text = re.sub(ur'\u00c7', ur'\u0197', text)
@@ -53,7 +64,7 @@ text = re.sub(r'\n +', r'\n', text)
 
 # tag speaker in plays
 text = re.sub(r'<sp>(\w:)', r'<sp><who>\1</who>', text)
-text = re.sub(r'<sp>([^<])', ur'<sp><nowho>\u00a0\u00a0\u2009</nowho>', text)
+text = re.sub(r'<sp>([^<])', ur'<sp><nowho>\u00a0\u00a0</nowho><glue width="0.2em" />\1', text)
 
 # usx -> sile
 text = re.sub(r'<usx .*?>',  r'<sile class="lectionary" papersize="a4">\n<include src="lectionary/styles.sil" />', text)
@@ -80,7 +91,28 @@ text = re.sub(r'<eject/>\s*', '', text, 1)
 text = re.sub(r'(<vfill/>\n)(<mt)', 
 	ur'\1<info category="h" value="\u00a0" />\n\2', text)
 text = re.sub(r'<info .*?/>\n', r'', text, 1)
+
+# make mt divider be bigskip`
+text = re.sub(r"(<mt.*\n)<b/>", r"\1<bigskip/>", text)
+
+parts = re.split(r'(<ms>.*)', text)
+for i in range(1,len(parts),2):
+	if i+1<len(parts) and parts[i].startswith("<ms>SAM"):
+		parts[i+1] = processSAM(parts[i+1])
+
+text = "".join(parts)
     
 with codecs.open("lectionary.xml", "w", "utf-8") as fd:
     fd.write(text)
     
+header = """<?xml version="1.0" encoding="utf-8"?>
+<sile class="lectionary" papersize="a4">
+<include src="lectionary/styles.sil" />
+"""
+
+parts = re.split(r"(<ms1)", text)
+
+for i in range(1,len(parts)-2,2):
+	parts[i+1] = parts[i+1] + "\n</sile>"
+	with codecs.open("pages/page" + str(i/2) + ".xml", "w", "utf-8") as fd:
+		fd.write(header + parts[i] + parts[i+1])
